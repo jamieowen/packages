1477ab32f3dcf57896b12fde74463d19eae323e5
Jamie Owen <hello@jamieowen.com>
2021-03-05 23:48:49 +0000

    refactor gpgpu examples

---
+++ examples/src/gpgpu-state.ts
@@ -0,0 +1,18 @@
+import {
+  Object3D,
+  Group,
+  Mesh,
+  MeshBasicMaterial,
+  BoxBufferGeometry,
+  WebGLRenderer,
+  Color,
+  BufferAttribute,
+} from "three";
+import {
+  sketch,
+  createStateTextureAst,
+  createDomeSimpleLight,
+  createDomeSimpleOpts,
+} from "@jamieowen/three";
+
+sketch(() => {});
